b0847aeb39555cbc5e5d200d27da9c46e033e048
Ekaterina Kulicheva <1bastard.child@gmail.com>
2025-06-12 13:37:21 +0700

    test changed

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -1,21 +1,27 @@
 package ru.netology.services;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class VacationServiceTest {
-
+    
     @Test
-    void shouldCalculateHoliday() {
+    public void testThreshold20000() {
+        
         VacationService service = new VacationService();
-
-        int balance = 0;
         int income = 10000;
         int expenses = 3000;
         int threshold = 20000;
+        int count = service.countVacation(income, expenses, threshold);
+        System.out.println(count);
+    }
 
-        long actual = service.countVacation(income, expenses, threshold);
-
-        Assertions.assertEquals(balance, threshold);
+    @Test
+    public void testThreshold150000() {
+        VacationService service = new VacationService();
+        int income = 100000;
+        int expenses = 60000;
+        int threshold = 150000;
+        int count = service.countVacation(income, expenses, threshold);
+        System.out.println(count);
     }
 }
